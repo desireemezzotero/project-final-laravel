c22b63e5307655f170199b1054499bac1d239e76
desireemezzotero <desireemezzotero1@gmail.com>
2025-05-06 14:37:35 +0200

    add crud index

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,18 @@
 @extends('/layouts/master')
 
 @section('component')
-<div class= "bg-black">
+<div class="bg-black">
   <div class="mx-auto container flex justify-items-center justify-center">
     @include('.components.card-login')
   </div>
+
+  @foreach ($films as $film)
+    <h1 class="text-white">{{ $film->title_film }}</h1>
+    <h2 class="text-white">
+      @foreach ($film->genres as $genre)
+        {{ $genre->title_genre }}
+      @endforeach
+    </h2>
+  @endforeach
 </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\FilmController;
+use App\Http\Controllers\FilmsController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,5 +16,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::resource('/film', FilmsController::class);
 
 require __DIR__ . '/auth.php';
