ab1cf3f8d9a6ffa594b263d77218692d09bba928
desireemezzotero <desireemezzotero1@gmail.com>
2025-05-06 15:45:14 +0200

    modify pages login and register

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,24 +5,33 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title> @if (Route::currentRouteName() == 'login')
+            Accedi
+           @else
+            Registrati
+           @endif</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
+          
     </head>
-    <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
-            <div>
-                <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-                </a>
-            </div>
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+    @include('.layouts.master')
+
+    <body class="font-sans bg-black">
+        <div class="flex flex-col items-center justify-items-center mt-10">
+            <h4 class="mb-4 text-4xl font-extrabold leading-none tracking-tigh md:text-3xl lg:text-5xl text-white pt-6">
+                @if (Route::currentRouteName() == 'login')
+                Accedi
+               @else
+                Registrati
+               @endif
+            </h4>
+            <div class="w-full sm:max-w-md mt-3 px-6 py-4 bg-black border-2 border-slate-500 shadow-md overflow-hidden sm:rounded-lg">
                 {{ $slot }}
             </div>
         </div>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,14 +2,17 @@
   <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
 
   <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
-    <img src="{{Vite::asset('resources/img/DM-logo.png')}}" class="h-8 rounded-full" alt="Flowbite Logo" />
+    <img src="{{asset('img/DM-logo.png')}}" class="h-8 rounded-full" alt="Flowbite Logo" />
   </a>
 
   <div class="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
 
       <button type="button" class="flex text-sm rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
         <span class="sr-only">Open user menu</span>
-        <img class="w-8 h-8 rounded-full" src="{{Vite::asset('resources/img/dmImgg.jpg')}}" alt="user photo">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+        </svg>
+        
       </button>
 
       <!-- Dropdown menu -->
@@ -35,7 +38,7 @@
   <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-user">
     <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0">
       <li>
-        <a href="#" class="block py-2 px-3 text-white rounded-sm md:bg-transparent" aria-current="page">Home</a>
+        <a href="{{route('film.index')}}" class="block py-2 px-3 text-white rounded-sm md:bg-transparent" aria-current="page">Home</a>
       </li>
 
       <li>
@@ -49,7 +52,7 @@
       @auth
   @if(auth()->user()->remember_token === auth()->user()->remember_token)
     <li>
-      <a href="{{ route('project.create') }}" class="block py-2 px-3 text-white rounded-sm md:bg-transparent" aria-current="page">Create</a>
+      <a href="#" class="block py-2 px-3 text-white rounded-sm md:bg-transparent" aria-current="page">Create</a>
     </li>
   @endif
 @endauth
